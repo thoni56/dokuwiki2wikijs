255d8ced32023dcf4193cd2c14b26ca0a5029e43
Thomas Nilefalk <thomas@nilefalk.se>
2021-02-17 20:18:55 +0100

    Fix tests to match current implementation

--- dokuwiki2wikijs_test.py
+++ dokuwiki2wikijs_test.py
@@ -1,23 +1,26 @@
 import unittest
 
-from dokuwiki2wikijs import unquote_markdown_headings, use_first_heading_or_filename_as_title, convert_filename_to_unicode
+from dokuwiki2wikijs import first_heading_or_filename, convert_filename_to_unicode
+
 
 class Dokuwiki2WikijsTest(unittest.TestCase):
 
-    def test_insert_title(self):
+    def test_get_title(self):
         lines = ["# Title"]
-        use_first_heading_or_filename_as_title(lines, "")
-        self.assertEqual(lines[1], "title: Title\n")
+        self.assertEqual(first_heading_or_filename(lines, ""), "Title")
 
         lines = ["# Title with spaces"]
-        use_first_heading_or_filename_as_title(lines, "")
-        self.assertEqual(lines[1], "title: Title with spaces\n")
+        self.assertEqual(first_heading_or_filename(
+            lines, ""), "Title with spaces")
 
         lines = ["some other text on the first line"]
-        use_first_heading_or_filename_as_title(lines, "Some other title")
-        self.assertEqual(lines[1], "title: Some other title\n")
+        self.assertEqual(first_heading_or_filename(
+            lines, "Some other title"), "Some other title")
 
     def test_convert_to_unicode(self):
-        self.assertEqual(convert_filename_to_unicode("a%C3%96bcdef"), "aÖbcdef")
-        self.assertEqual(convert_filename_to_unicode("abc%C3%B6def"), "abcödef")
-        self.assertEqual(convert_filename_to_unicode("a%C3%B6bcdef"), "aöbcdef")
+        self.assertEqual(convert_filename_to_unicode(
+            "a%C3%96bcdef"), "aÖbcdef")
+        self.assertEqual(convert_filename_to_unicode(
+            "abc%C3%B6def"), "abcödef")
+        self.assertEqual(convert_filename_to_unicode(
+            "a%C3%B6bcdef"), "aöbcdef")
